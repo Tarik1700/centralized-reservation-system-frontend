d92d48732e932ff4ed24e654ef435b1630077ffa
KahrimanRr <102815359+KahrimanRr@users.noreply.github.com>
2023-03-27 21:16:23 +0200

    GRA-14, Create Landing page (#2)
    * created Nav bar,side bar,search form,added responsive in tailwindcss and added carousel templ
    
    * more responsive, added carousel button and label
    
    * added card component,added bar icon, dividor  line
    
    * added images,uplodaded search
    
    * added new file Slider, previous one Popular is not used, updated Render file
    
    * fixed minor issues, added padding and divider
    
    * added hover below Navbar, added footer on Slides, reduced gaps
    
    ---------
    
    Co-authored-by: Rijad Kahriman <rijadkahr@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      extend: {},
       colors: {
         primary: {
           50: "#eff6ff",
